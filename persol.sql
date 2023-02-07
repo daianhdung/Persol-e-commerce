66d0c74cae9d2ac810dad087bc68c53cf25e569a
daianhdung <daianhdung99@gmail.com>
2023-02-08 00:42:22 +0700

    add database file, folder Be_laravel

---
+++ persol.sql
@@ -0,0 +1,98 @@
+CREATE TABLE `role` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `description` VARCHAR(100)
+);
+
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `email` varchar(255) UNIQUE,
+  `password` varchar(255),
+  `fullname` varchar(100),
+  `phone` varchar(100),
+  `address` varchar(100),
+  `date` datetime DEFAULT (now()),
+  `role_id` INT
+);
+
+CREATE TABLE `category` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `brand` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE `product` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `main_image` varchar(100),
+  `amount_of_sold` int DEFAULT 0,
+  `price` int,
+  `category_id` int,
+  `brand_id` int
+);
+
+CREATE TABLE `image_product` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `product_id` int
+);
+
+CREATE TABLE `bookmark_product` (
+  `user_id` int,
+  `product_id` int,
+  PRIMARY KEY (`user_id`, `product_id`)
+);
+
+CREATE TABLE `coupon` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(50) UNIQUE NOT NULL,
+  `rate` float NOT NULL
+);
+
+CREATE TABLE `status` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(50)
+);
+
+CREATE TABLE `order` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `fee_ship` int,
+  `coupon` float,
+  `temp_total` int,
+  `total` float,
+  `user_id` int,
+  `delivery_address` varchar(255),
+  `status_id` int
+);
+
+CREATE TABLE `product_order` (
+  `order_id` int,
+  `product_id` int,
+  `amount` int,
+  `price` int,
+  PRIMARY KEY (`order_id`, `product_id`)
+);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+
+ALTER TABLE `product` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+ALTER TABLE `product` ADD FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`);
+
+ALTER TABLE `image_product` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `bookmark_product` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `bookmark_product` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `order` ADD FOREIGN KEY (`status_id`) REFERENCES `status` (`id`);
+
+ALTER TABLE `order` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `product_order` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
+
+ALTER TABLE `product_order` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
