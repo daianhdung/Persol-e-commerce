be1b283b42b30f378493c3b1172258028d9c8196
tri nguyen <hoangtri272@gmail.com>
2023-02-18 21:31:53 +0700

    add db

---
+++ backend_laravel/app/Http/Controllers/api/ProductsController.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Http\Controllers\api;
+use App\Models\Product;
+use App\Http\Controllers\ResponseController;
+use PhpParser\Node\Expr\Isset_;
+use Illuminate\Http\Request;
+
+class ProductsController extends ResponseController{
+    public function show(){
+        $product = Product::all();
+        return $this->successResponse($product,'thanh cong');
+
+    }
+
+    public function showbyId($id) {
+        $product = Product::find($id);
+        if(is_null($product)) {
+            return response()->json(['message' => 'Product not found']);
+        }
+        return response()->json($product::find($id),200);
+    }
+
+    public function delete(Product $product,$id){
+        $product = Product::find($id);
+        if(is_null($product)) {
+            return response()->json(['message' => 'Product not found']);
+        }
+        $product->delete();
+        return $this->successResponse($product,'thanh cong');
+
+    }
+
+
+    public function addproduct(Request $request) {
+        $product = Product::create($request->all());
+        return response($product,201);
+    }
+
+
+
+    public function updateproduct(Request $request, $id){ 
+        $product= Product::find($id);
+        if(is_null($product)) {
+            return response()->json(['message' => 'Product not found']);
+        }
+        $product -> update($request->all());
+        return response($product,200);
+
+    }
+
+    
+
+}
--- backend_laravel/app/Http/Controllers/api/UserController.php
+++ backend_laravel/app/Http/Controllers/api/UserController.php
@@ -18,4 +18,26 @@ class UserController extends ResponseController
 
         return $this->successResponse($user, 'Đăng ký tài khoản thành công !');
     }
+
+    public function deleteUser(User $user,$id){
+        $user = User::find($id);
+        if(is_null($user)) {
+            return response()->json(['message' => 'Product not found']);
+        }
+        $user->delete();
+        return $this->successResponse($user,'thanh cong');
+
+    }
+
+    //sua lai add user
+    
+    public function addproduct(Request $request) {
+        $user = User::create($request->all());
+        return response($user,201);
+    }
+
+
+
+
+
 }
